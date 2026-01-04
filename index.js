4f5dd38020d16c826b87d766bf4c9ff12d492a50
Iamutkarshkumar <uksagar2005@gmail.com>
2026-01-04 20:58:59 +0530

    cors updated

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:"http://localhost:5173",
+    origin:"https://expense-tracker-plum-three-83.vercel.app/",
     credentials:true
 }
 app.use(cors(corsOptions));
