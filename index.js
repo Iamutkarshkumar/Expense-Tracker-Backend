b63e6b60c4942f88bdd1f565d4f18d6e9badf08f
Iamutkarshkumar <uksagar2005@gmail.com>
2026-06-25 09:44:09 +0530

    index.js fix

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express, { urlencoded } from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 import connectDB from "./database/db.js";
 import userRoute from "./routes/user.route.js";
 import expenseRoute from "./routes/expense.route.js";
@@ -28,6 +29,13 @@ app.use(cors(corsOptions));
 app.use("/api/v1/user",userRoute);
 app.use("/api/v1/expense",expenseRoute);
 
+// Serve static files from frontend dist folder
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+// Fallback: serve index.html for all non-API routes (for React Router)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 
 app.listen(PORT,()=>{
     connectDB();
